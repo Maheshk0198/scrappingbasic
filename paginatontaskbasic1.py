15f7fa3316b876c543456ca59f1fecaf192d3491
Maheshk0198 <169039028+Maheshk0198@users.noreply.github.com>
2024-06-03 14:36:09 +0530

    Create paginatontaskbasic1.py
    the number of page list already present in page.

---
+++ paginatontaskbasic1.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.scrapethissite.com/pages/forms/?page_num={}'
+teamgoallist = []
+with open('teamgoallist.txt', 'w') as file:
+    for i in range(1,26):
+        urlpage = url.format(i)
+        response = requests.get(urlpage)
+        page = response.content
+        soup = BeautifulSoup(page, 'lxml')
+        tablerows = soup.find_all('tr', class_ ='team')
+        for tabledata in tablerows:
+            teamname = tabledata.find_next('td', class_='name').text.strip()
+            teamgoal = tabledata.find_next('td', class_='gf').text.strip()
+            teamgoallist.append(teamname + '-' + teamgoal)
+            file.write(teamname + '-' + teamgoal + '\n')
